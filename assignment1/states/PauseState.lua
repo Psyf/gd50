63738fbb69606cddea59c7459d6392f21e236562
Psyf <gzsaif001@gmail.com>
2020-07-09 09:41:25 +0800

    background music pause

--- assignment1/states/PauseState.lua
+++ assignment1/states/PauseState.lua
@@ -3,6 +3,10 @@ PauseState = Class{__includes = BaseState}
 pauseIcon = love.graphics.newImage('pause.png') 
 
 function PauseState:init() 
+
+    -- pause backgroun music as soon as you pause
+    sounds['music']:pause()
+
     -- save PlayState vars
     self.x = 0 
 end 
@@ -10,7 +14,12 @@ end
 function PauseState:update(dt) 
     -- if p is pressed again, go back to play
     if love.keyboard.wasPressed('p') then
+
+        -- ask play state to reload saved variables
         gStateMachine:change('play')
+
+        -- resume background music 
+        sounds['music']:resume()
     end
 end 
 
